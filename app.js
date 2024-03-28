dc4bfcfe1594d83c29a6c666b59ff1d76a81e296
isaacgomu <IsaacDalglish03@gmail.com>
2024-03-28 18:41:06 +0000

    added a few more things, cant seem to limit on buying if under the required hits

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const punchBtn = document.getElementById("punchBtn");
 const luffyBtn = document.getElementById("luffyBtn");
 const hitsSpan = document.getElementById("hitsSpan");
 const hpsSpan = document.getElementById("hpsSpan");
+const zoroBtn = document.getElementById("zoroBtn");
+const luffysSpan = document.getElementById("luffysSpan");
+const zorosSpan = document.getElementById("zorosSpan");
+const disable = (e) => {
+  e.disabled = true;
+};
 
 const stats = {
   hitCount: 0,
   hps: 0,
+  luffys: 0,
+  zoros: 0,
 };
 
 const storageStats = JSON.parse(localStorage.getItem("stats"));
@@ -13,6 +21,8 @@ const storageStats = JSON.parse(localStorage.getItem("stats"));
 if (storageStats !== null) {
   stats.hitCount = storageStats.hitCount;
   stats.hps = storageStats.hps;
+  stats.luffys = storageStats.luffys;
+  stats.zoros = storageStats.zoros;
 }
 
 function hitEnemy() {
@@ -24,6 +34,15 @@ function hitEnemy() {
 function buyLuffy() {
   stats.hps++;
   stats.hitCount -= 10;
+  stats.luffys++;
+  updatePage();
+  updateStorage();
+}
+
+function buyZoro() {
+  stats.hps += 10;
+  stats.hitCount -= 1000;
+  stats.zoros++;
   updatePage();
   updateStorage();
 }
@@ -31,6 +50,8 @@ function buyLuffy() {
 function updatePage() {
   hitsSpan.textContent = stats.hitCount;
   hpsSpan.textContent = stats.hps;
+  luffysSpan.textContent = stats.luffys;
+  zorosSpan.textContent = stats.zoros;
 }
 
 function updateStorage() {
@@ -39,9 +60,16 @@ function updateStorage() {
 
 punchBtn.addEventListener("click", hitEnemy);
 luffyBtn.addEventListener("click", buyLuffy);
+zoroBtn.addEventListener("click", buyZoro);
 
 setInterval(function () {
   stats.hitCount += stats.hps;
   updatePage();
   updateStorage();
 }, 1000);
+
+if (stats.hitsSpan < 10) {
+  disable(luffyBtn);
+} else {
+  enable(luffyBtn);
+}
