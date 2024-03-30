8bcb52714e411f3bb66bd5edd401d92a3cf1d302
isaacgomu <IsaacDalglish03@gmail.com>
2024-03-30 09:38:23 +0000

    added reset button, disabled buttons when cannot afford, added sound to hitting enemy, added animation to hitting enemy

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const hpsSpan = document.getElementById("hpsSpan");
 const zoroBtn = document.getElementById("zoroBtn");
 const luffysSpan = document.getElementById("luffysSpan");
 const zorosSpan = document.getElementById("zorosSpan");
-const disable = (e) => {
-  e.disabled = true;
+const resetBtn = document.getElementById("resetBtn");
+const Enemy = document.getElementById("enemyimage");
+const disable = (btn) => {
+  btn.disabled = true;
+  updatePage();
+  updateStorage();
 };
-
+const enable = (btn) => {
+  btn.disabled = false;
+  updatePage();
+  updateStorage();
+};
 const stats = {
   hitCount: 0,
   hps: 0,
@@ -40,7 +48,7 @@ function buyLuffy() {
 }
 
 function buyZoro() {
-  stats.hps += 10;
+  stats.hps += 100;
   stats.hitCount -= 1000;
   stats.zoros++;
   updatePage();
@@ -58,9 +66,27 @@ function updateStorage() {
   localStorage.setItem("stats", JSON.stringify(stats));
 }
 
-punchBtn.addEventListener("click", hitEnemy);
+function Reset() {
+  stats.hitCount = 0;
+  stats.hps = 0;
+  stats.luffys = 0;
+  stats.zoros = 0;
+  updatePage();
+  updateStorage();
+}
+
+function punchsound() {
+  let audio = new Audio("./assets/luffypunch.mp3");
+  audio.play();
+}
+
+punchBtn.addEventListener("click", function () {
+  hitEnemy();
+  punchsound();
+});
 luffyBtn.addEventListener("click", buyLuffy);
 zoroBtn.addEventListener("click", buyZoro);
+resetBtn.addEventListener("click", Reset);
 
 setInterval(function () {
   stats.hitCount += stats.hps;
@@ -68,8 +94,20 @@ setInterval(function () {
   updateStorage();
 }, 1000);
 
-if (stats.hitsSpan < 10) {
-  disable(luffyBtn);
-} else {
-  enable(luffyBtn);
-}
+setInterval(function () {
+  if (stats.hitCount <= 9) {
+    disable(luffyBtn);
+  } else {
+    enable(luffyBtn);
+  }
+  if (stats.hitCount <= 999) {
+    disable(zoroBtn);
+  } else {
+    enable(zoroBtn);
+  }
+}, 1);
+
+setInterval(function () {
+  if (stats.hitCount <= 1000) {
+  }
+});
